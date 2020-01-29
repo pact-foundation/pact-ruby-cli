235ed2a076db7a13081ab1072ed9a4fe6c6ec190
Beth Skurrie <beth@bethesque.com>
2020-01-30 08:58:25 +1100

    chore: create script to update bundle on docker

---
+++ script/update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+rm -rf tmp/Gemfile.lock
+docker build . -f Dockerfile-bundle-base -t pact_cli_bundle_base
+docker run -it --rm -v ${PWD}/tmp:/tmp/gemfiles pact_cli_bundle_base:latest sh -c "bundle update && cp Gemfile.lock /tmp/gemfiles/Gemfile.lock"
+mv tmp/Gemfile.lock .
