566ea37bb0229f50ff95d410846cc33ffc00bff7
Beth Skurrie <beth@bethesque.com>
2020-01-30 09:02:15 +1100

    chore: don't output docker build info

--- script/update.sh
+++ script/update.sh
@@ -2,6 +2,6 @@
 
 set -e
 rm -rf tmp/Gemfile.lock
-docker build . -f Dockerfile-bundle-base -t pact_cli_bundle_base
+docker build . -f Dockerfile-bundle-base -t pact_cli_bundle_base >/dev/null 2>&1
 docker run -it --rm -v ${PWD}/tmp:/tmp/gemfiles pact_cli_bundle_base:latest sh -c "bundle update && cp Gemfile.lock /tmp/gemfiles/Gemfile.lock"
 mv tmp/Gemfile.lock .
