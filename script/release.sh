ff1ba90ed40bd4c22e29e4d39bda5e02bd76840c
Beth Skurrie <beth@bethesque.com>
2020-02-13 13:39:41 +1100

    chore: update script/release.sh

--- script/release.sh
+++ script/release.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# THIS IS A MESS and doesn't work.
+
 set -e
 
 unset X_PACT_DEVELOPMENT
@@ -11,9 +13,12 @@ bundle exec rake package:update
 if git log -1 | grep "feat(gems)"; then
   bundle exec bump ${1:-minor} --no-commit
   bundle exec rake generate_changelog
-  git add lib/pact/cli/version.rb
-  git commit -m "chore(release): version $(cat VERSION)" && git push origin master
-  bundle exec rake tag_for_release
+  git add lib/pact/cli/version.rb CHANGELOG.md Gemfile.lock
+  VERSION=$(ruby -I lib -e "require 'pact/cli/version.rb'; puts Pact::Cli::VERSION")
+  git commit -m "chore(release): version ${VERSION} [ci-skip]" && git push origin master
+  RELEASE=1
+  TAG="${VERSION}-${RELEASE}"
+  git tag -a ${TAG} -m "chore(release): version ${TAG}" && git push origin ${TAG}
 else
   echo "No gems updated, not releasing"
 fi
