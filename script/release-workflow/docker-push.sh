910b4812ae1d03f784fb11a97f622f30cc020658
Beth Skurrie <beth@bethesque.com>
2021-08-19 16:28:33 +1000

    chore: disable pushing to 0 tag

--- script/release-workflow/docker-push.sh
+++ script/release-workflow/docker-push.sh
@@ -2,11 +2,12 @@
 
 set -euo >/dev/null
 
-if [ -n "${MAJOR_TAG}" ]; then
-  docker tag ${DOCKER_IMAGE_ORG_AND_NAME}:latest ${DOCKER_IMAGE_ORG_AND_NAME}:${MAJOR_TAG}
-  docker push ${DOCKER_IMAGE_ORG_AND_NAME}:${MAJOR_TAG}
-  docker push ${DOCKER_IMAGE_ORG_AND_NAME}:latest
-fi
+# Enable when we bump the major version to 1 (which we should)
+# if [ -n "${MAJOR_TAG}" ]; then
+#   docker tag ${DOCKER_IMAGE_ORG_AND_NAME}:latest ${DOCKER_IMAGE_ORG_AND_NAME}:${MAJOR_TAG}
+#   docker push ${DOCKER_IMAGE_ORG_AND_NAME}:${MAJOR_TAG}
+#   docker push ${DOCKER_IMAGE_ORG_AND_NAME}:latest
+# fi
 
 docker tag ${DOCKER_IMAGE_ORG_AND_NAME}:latest ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG}
 docker push ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG}
