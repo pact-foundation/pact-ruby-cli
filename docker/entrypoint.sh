ca474f9c7ae23fb60c84bdfa637d1ec08ba7a852
Yousaf Nabi <yousaf@pactflow.io>
2022-10-18 23:26:09 +0100

    fix: branch detection fix in response git cve fix (#79)
    git branch detection command fails due to cve fix https://github.blog/2022-04-12-git-security-vulnerability-announced/
    > fatal: unsafe repository (REPO is owned by someone else) in other workflow steps after running checkout
    See https://github.com/actions/checkout/issues/766

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,11 @@
 
 PACT_COMMANDS=" broker help mock-service publish stub-service verify version "
 
+# git branch detection command fails due to cve fix https://github.blog/2022-04-12-git-security-vulnerability-announced/
+# fatal: unsafe repository (REPO is owned by someone else) in other workflow steps after running checkout
+# https://github.com/actions/checkout/issues/766
+git config --global --add safe.directory "*" 
+
 if [ -n "$1" ] && echo "$PACT_COMMANDS" | grep -F " $1 " > /dev/null 2>&1 ; then
   # Make the pact content dynamic so a new version gets published every time
   if echo "$@" | grep "/pact/example/pacts" >/dev/null 2>&1 ; then
