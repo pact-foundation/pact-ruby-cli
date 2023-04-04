56430dbab8acae6d5025f60921779906e9504d16
Yousaf Nabi <you@saf.dev>
2023-04-04 20:08:37 +0100

    chore: add Cross-platform emulator collection - qemu

--- script/release-workflow/docker-prepare.sh
+++ script/release-workflow/docker-prepare.sh
@@ -2,5 +2,6 @@
 
 set -euo >/dev/null
 
+docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx create --platform linux/arm64,linux/arm/v8,linux/amd64 --name multi_arch_builder
 docker buildx use multi_arch_builder
