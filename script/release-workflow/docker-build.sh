065daf90dda66d25c00453f42cae89b878bec9e0
Yousaf Nabi <you@saf.dev>
2023-04-04 20:03:44 +0100

    chore: only scan amd64 image, due to x86_64 runner

--- script/release-workflow/docker-build.sh
+++ script/release-workflow/docker-build.sh
@@ -9,7 +9,8 @@ set -euo >/dev/null
 ## This will allow for local use for testing or scanning with trivy (multi-manifest builds cannot be imported)
 ## we will build a multi-manifest build during ./docker-push.sh
 
-for arch in amd64 arm64 arm; do 
+# for arch in amd64 arm64 arm; do 
+for arch in amd64; do 
     docker buildx build \
     --platform linux/$arch \
     --output type=docker \
