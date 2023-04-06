1b4f2f6dca932e60fd03f135788571f9d61be423
Yousaf Nabi <you@saf.dev>
2023-04-06 14:01:28 +0100

    chore: rm newline

--- script/scan-inside-docker-container.sh
+++ script/scan-inside-docker-container.sh
@@ -4,5 +4,4 @@ set -eu
 # TODO set exit code back to 1
 
 wget -q -O - https://raw.githubusercontent.com/aquasecurity/trivy/master/contrib/install.sh | sh -s -- -b /usr/local/bin
-
 trivy filesystem --exit-code 0 --no-progress /
