6321fbfec68aad34b8cd85bbeb39ee0d5b73f8d4
Yousaf Nabi <you@saf.dev>
2023-04-11 17:21:19 +0100

    ci(test): arm64 linux test + run all cli commands

--- script/test.sh
+++ script/test.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-docker build . -t pactfoundation/pact-cli
-docker run --rm pactfoundation/pact-cli:latest
+DOCKER_IMAGE_ORG_AND_NAME=${DOCKER_IMAGE_ORG_AND_NAME:-"you54f/pact-cli"}
+TAG=${TAG:-"latest"}
+
+docker build . -t ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG}
+docker run --rm ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG}
+docker run --rm ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG} version
+docker run --rm ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG} help
+docker run --rm ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG} mock-service --help
+docker run --rm ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG} pact-broker --help
+docker run --rm ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG} pactflow --help
+docker run --rm ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG} publish --help
+docker run --rm ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG} stub-service --help
+docker run --rm ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG} verify --help
