75a481daa3df0fae9b85a5e0d0fd8912a7899117
Yousaf Nabi <you@saf.dev>
2023-04-26 16:45:24 +0100

    chore(test): update test commands in built image

--- script/test-node.sh
+++ script/test-node.sh
@@ -4,11 +4,10 @@
 echo building for ${NODE_VERSION}
 
 docker build . -t pactfoundation/pact-cli:latest-node${NODE_VERSION} -f Dockerfile-node
-docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} pact
+docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} --help
+docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} mock-service --help
 docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} pact-broker --help
-docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} pact-message --help
-docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} pact-mock-service --help
-docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} pact-plugin-cli --help
-docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} pact-provider-verifier --help
-docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} pact-stub-service --help
 docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} pactflow --help
+docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} publish --help
+docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} stub-service --help
+docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} verify --help
