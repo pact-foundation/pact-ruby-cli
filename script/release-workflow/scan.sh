ca67c82fd8d347c31c1e9926a4d4115aa710153a
Yousaf Nabi <yousafn@gmail.com>
2023-05-17 21:09:26 +0100

    Revert "feat: Multi manifest build - arm64/arm (new) + amd64 (current) (#98)"
    This reverts commit 944b799a50ca512097c9c2bd0f56ad5ae9de2e83.

--- script/release-workflow/scan.sh
+++ script/release-workflow/scan.sh
@@ -4,11 +4,9 @@ set -eu
 
 SCRIPT_DIR=$(cd "$(dirname $0)"/.. && pwd)
 
-for arch in amd64; do 
-    docker run --rm \
-      -v ${PWD}/script/scan-inside-docker-container.sh:/pact/scan-inside-docker-container.sh \
-      -u root \
-      --entrypoint /bin/sh \
-      "${DOCKER_IMAGE_ORG_AND_NAME}:latest-${arch}" \
-      /pact/scan-inside-docker-container.sh
-done
+docker run --rm \
+  -v ${PWD}/script/scan-inside-docker-container.sh:/pact/scan-inside-docker-container.sh \
+  -u root \
+  --entrypoint /bin/sh \
+  "${DOCKER_IMAGE_ORG_AND_NAME}:latest" \
+  /pact/scan-inside-docker-container.sh
