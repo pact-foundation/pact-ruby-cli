3f7aae28fbc152aea0143bda36dfbfeedcae88e3
Yousaf Nabi <yousaf@pactflow.io>
2024-01-23 16:06:45 +0000

    chore(ci): remove dupe cmd now in new script

--- script/release-workflow/git-push.sh
+++ script/release-workflow/git-push.sh
@@ -2,10 +2,6 @@
 
 set -euo >/dev/null
 
-if [[ $(git status --porcelain) ]]; then
-    git add Gemfile.lock VERSION lib/pact/cli/version.rb
-    git commit -m "chore(release): version ${TAG}"
-fi
 git tag -a "${TAG}" -m "chore(release): version ${TAG}"
 git push origin ${TAG}
 git push origin master
