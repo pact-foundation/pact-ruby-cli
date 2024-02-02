9b3e70eb09348c29abc08c24c69810904ca4b233
Yousaf Nabi <you@saf.dev>
2024-02-02 19:39:18 +0000

    feat(ci): additionally publish images to ghcr.io

--- script/release-workflow/docker-push.sh
+++ script/release-workflow/docker-push.sh
@@ -6,6 +6,9 @@ push() {
   docker buildx build --platform=linux/amd64,linux/arm64,linux/arm \
     --output=type=image,push=true \
     -t ${DOCKER_IMAGE_ORG_AND_NAME}:$1 .
+  docker buildx build --platform=linux/amd64,linux/arm64,linux/arm \
+  --output=type=image,push=true \
+  -t ghcr.io/${DOCKER_IMAGE_ORG_AND_NAME}:$1 .
 }
 
 if [ -n "${MAJOR_TAG:-}" ]; then
