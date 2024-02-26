b98c92fb9fa6cd0c5678ac55a228bfe1757944a5
Yousaf Nabi <you@saf.dev>
2024-02-26 14:40:01 +0000

    chore: update docker-push.sh to match pact-broker-docker

--- script/release-workflow/docker-push.sh
+++ script/release-workflow/docker-push.sh
@@ -2,21 +2,29 @@
 
 set -euo >/dev/null
 
+## Publish a multi arch build
+## ($TAG||$MAJOR_TAG||$LATEST)
 push() {
+  ## These will use cached builds, so wont build every time.
   docker buildx build --platform=linux/amd64,linux/arm64,linux/arm \
     --output=type=image,push=true \
     -t ${DOCKER_IMAGE_ORG_AND_NAME}:$1 .
+}
+push_ghcr() {
   docker buildx build --platform=linux/amd64,linux/arm64,linux/arm \
   --output=type=image,push=true \
-  -t ghcr.io/${DOCKER_IMAGE_ORG_AND_NAME}:$1 .
+  -t ghcr.io/${DOCKER_IMAGE_ORG_AND_NAME}:$1 
 }
 
 if [ -n "${MAJOR_TAG:-}" ]; then
   push ${MAJOR_TAG}
+  push_ghcr ${MAJOR_TAG}
 fi
 
 push ${TAG}
+push_ghcr ${TAG}
 
 if [ "${PUSH_TO_LATEST}" != "false" ]; then
   push latest
+  push_ghcr latest
 fi
