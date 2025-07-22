4b829b5a40fcce50d154d46b36076170c918dd21
Yousaf Nabi <yousaf@pactflow.io>
2025-07-22 17:28:55 +0100

    chore(ci): ensure docker build script fails on error

--- script/test-node.sh
+++ script/test-node.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 
 
 echo building for ${NODE_VERSION}
 
 docker build . -t pactfoundation/pact-cli:latest-node${NODE_VERSION} -f Dockerfile-node
-docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} --help
+docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} help
 docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} mock-service --help
 docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} pact-broker --help
 docker run --rm pactfoundation/pact-cli:latest-node${NODE_VERSION} pactflow --help
